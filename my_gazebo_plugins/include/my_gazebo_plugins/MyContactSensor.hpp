161e624ad9ccd10eabaf846c5cd056286e1c3793
rua0ra1 <icherryme.0407@gmail.com>
2024-07-19 10:18:13 +1000

    completed the implementation of contact plugin

--- my_gazebo_plugins/include/my_gazebo_plugins/MyContactSensor.hpp
+++ my_gazebo_plugins/include/my_gazebo_plugins/MyContactSensor.hpp
@@ -2,19 +2,25 @@
 #define MYCONTACTSENSOR_HPP
 
 #include <gazebo/gazebo.hh>
-#include <gazebo/sensors/ContactSensor.hh>
+#include <gazebo/sensors/sensors.hh>
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 
+
 namespace gazebo {
 class MyContactSensor : public SensorPlugin {
-public:
-  MyContactSensor() : SensorPlugin(){};
-  void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
-  void OnUpdate();
+   public:
+     MyContactSensor() : SensorPlugin() {}
+    void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
+    void OnUpdate();
 
-private:
+   private:
+     sensors::ContactSensorPtr parentSensor;
+     event::ConnectionPtr updateConnection;
 
+     ros::Publisher contactPublisher_;
+     std_msgs::Bool contactMsg_;
+     bool lastState_;
 };
 } // namespace gazebo
 
---
+++ my_gazebo_plugins/src/MyContactSensor.cpp
@@ -0,0 +1,60 @@
+#include "my_gazebo_plugins/MyContactSensor.hpp"
+#include <tbai_core/Types.hpp>
+#include <tbai_core/config/YamlConfig.hpp>
+
+namespace gazebo{
+GZ_REGISTER_SENSOR_PLUGIN(MyContactSensor)
+
+
+/**********************************************************************************************************************/
+/**********************************************************************************************************************/
+/**********************************************************************************************************************/
+
+void MyContactSensor::Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf) {
+    this->parentSensor = std::dynamic_pointer_cast<sensors::ContactSensor>(sensor);
+
+    if (!this->parentSensor) {
+        ROS_ERROR_STREAM("[ContactSensor] Could not load contact sensor plugin.");
+        return;
+    }
+
+    this->updateConnection = this->parentSensor->ConnectUpdated(std::bind(&MyContactSensor::OnUpdate, this));
+    this->parentSensor->SetActive(true);
+
+    // create ROS publisher
+    const std::string topicName = this->parentSensor->Name();
+    contactPublisher_ = ros::NodeHandle().advertise<std_msgs::Bool>(topicName, 1);
+
+    // Set initial state
+    lastState_ = false;
+
+    ROS_INFO_STREAM("[ContactSensor] Loading ContactSensor plugin. Publishing on topic /" << topicName);
+
+    // Setup update rate
+    auto updateRate = tbai::core::fromRosConfig<tbai::scalar_t>("contact_sensor/update_rate");
+    this->parentSensor->SetUpdateRate(updateRate);
+
+    ROS_INFO_STREAM("[ContactSensor] Loaded ContactSensor plugin. Update rate: " << updateRate << " Hz");
+}
+
+/**********************************************************************************************************************/
+/**********************************************************************************************************************/
+/**********************************************************************************************************************/
+
+void MyContactSensor::OnUpdate() {
+    bool state = parentSensor->Contacts().contact_size()!=0;
+    // if state didn't change use
+    if(state==lastState_){
+        return;
+    }
+
+    //contact flag changed,publish message
+    contactMsg_.data=state;
+    contactPublisher_.publish(contactMsg_);
+    lastState_=state;
+}
+
+
+}
+
+
